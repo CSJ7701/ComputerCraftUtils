de2ffffea712693eb15e8499dfc3b3ff0619f859
Christian <c.ship.johnson@gmail.com>
2025-01-03 12:40:28 -0500

    Added addon vars

--- Utils/BareInstall.lua
+++ Utils/BareInstall.lua
@@ -146,6 +146,8 @@ end
 -- Function to check and set environment variables
 local function env_setup()
    local env_defaults = {["createos.repo"] = "https://raw.githubusercontent.com/CSJ7701/ComputerCraftUtils/", ["createos.branch"] = "main", ["createos.module_dir"] = "/Modules/"}
+   local env_addons = {["shell.package_path"] = "/modules/?/init.lua"}
+   
    for var_name, default_value in pairs(env_defaults) do
       -- Check if the variable already has a value
       local current_value = settings.get(var_name)  -- os.getenv retrieves the environment variable value
@@ -155,6 +157,15 @@ local function env_setup()
 	 print("[LOG] -- Set " .. var_name .. " to " .. default_value)
       else
 	 print("[LOG] -- " .. var_name .. " is already set to " .. current_value)
+      end
+   end
+
+   for var_name, var_val in pairs(env_defaults) do
+      local current_value = settings.get(var_name)
+      if current_value or not current_value == "" then
+	 settings.set(var_name, current_value .. ";" .. var_val)
+      else
+	 settings.set(var_name, var_val)
       end
    end
 end
