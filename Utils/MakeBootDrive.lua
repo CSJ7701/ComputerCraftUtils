7a3f6f257cafdab10670cc5ec2796f74f46d6536
Christian Johnson <c.ship.johnson@gmail.com>
2025-01-05 09:52:37 -0500

    MakeDiskDrive: Added env_setup

--- Utils/MakeBootDrive.lua
+++ Utils/MakeBootDrive.lua
@@ -117,9 +117,38 @@ local function main()
     fetchAndDownloadDirectory(bootdriveApiUrl, basePath)
 end
 
+--- Function to check and setup environment variables
+local function env_setup()
+   local env_defaults = {["createos.repo"] = "CSJ7701/ComputerCraftUtils", ["createos.branch"] = "main", ["createos.module_dir"] = "Modules/"}
+   local env_addons = {["shell.package_path"] = "/modules/?/init.lua"}
+   
+   for var_name, default_value in pairs(env_defaults) do
+      -- Check if the variable already has a value
+      local current_value = settings.get(var_name)  -- os.getenv retrieves the environment variable value
+      if not current_value or current_value == "" then
+	 -- Set the variable to the default value
+	 settings.set(var_name, default_value)  -- os.setenv sets the environment variable
+	 print("[LOG] -- Set " .. var_name .. " to " .. default_value)
+      else
+	 print("[LOG] -- " .. var_name .. " is already set to " .. current_value)
+      end
+   end
+
+   for var_name, var_val in pairs(env_addons) do
+      local current_value = settings.get(var_name)
+      if current_value or not current_value == "" then
+	 settings.set(var_name, current_value .. ";" .. var_val)
+      else
+	 settings.set(var_name, var_val)
+      end
+   end
+   settings.save()
+end	 
+
 if clearDisk() == 1 then
     print("[LOG] -- Attempting Download")
     main()
+    env_setup()
 else
     print("[LOG] -- Download aborted, /disk could not be cleared.")
 end
